2fa8a596d2c192e2128a09f15d9250fb599533da
Ethan Meitz <ejmeitz1@gmail.com>
2020-08-10 12:38:10 -0500

    working to make p5 animations responsive

--- front_end/src/Components/fun.js
+++ front_end/src/Components/fun.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Container} from 'react-bootstrap';
 import styled from 'styled-components';
 
 const FunStyles = styled.div `
@@ -8,49 +9,36 @@ const FunStyles = styled.div `
     padding:0;
     box-sizing:border-box;
 }
-.p5Canvas{
-    margin:auto;
-}
-
-.body{
-   display:flex;
-   justify-content:center;
-   margin: 60px auto 1vh auto;
-}
 .grid{
     display:grid;
-    margin-top:20px;
+    margin-top:60px;
     margin-bottom:20px;
     padding-bottom:30px;
     width: 80%;
     grid-template-columns: 40% 40%;
     grid-gap:20px;
-    align-items:strech;
+ 
     justify-items:center;
     justify-content:center;
     grid-auto-flow:row;
 }
+
 .animation{
     justify-items:center;
     justify-content:center;
     width:500px;
-    height:550px;
+    height:500px;
     padding: 20px;
 }
 .animation h2{
     margin-bottom:10px;
 }
 
-iframe{
-    width: 500px;
-    height: 500px;
-    overflow:hidden;
-}
 
 @media screen and (max-width: 1600px) {
     .grid{
         display:grid;
-        margin:auto;
+        margin: 60px auto;
         grid-template-columns: 80%;
         width: 80%;
         grid-gap:20px;
@@ -61,6 +49,14 @@ iframe{
         grid-auto-flow:row;
     }
 
+    .animation{
+        justify-items:center;
+        justify-content:center;
+        width:500px;
+        height:500px;
+        padding: 20px;
+    }
+
 }
 
 @media screen and (max-width: 900px) {
@@ -68,14 +64,14 @@ iframe{
     {
        width:300px;
        height:300px;
-        
     }
+
     .grid{
         display:grid;
         width: 80%;
         grid-gap:10px;
        
-        margin:auto;
+        margin: 60px auto;
     
         justify-items:center;
         justify-content:center;
@@ -103,28 +99,21 @@ export default class my_projects extends Component {
     render(){
         return (
     <>
-        <FunStyles>
-            <div className = "body">
+        <FunStyles>  
                 <main className = "grid">
-                    <div className = "animation">
+                    <Container>
                         <h2 className = "title">Bouncing Balls</h2>
-                        <iframe  title = "bouncing-balls" scrolling="no" frameBorder="0" src="https://editor.p5js.org/ejmeitz/embed/Ggf2mFF9o"></iframe>
-                    </div>
-                    <div className = "animation">
-                        <h2 className = "title">Bouncing Balls</h2>
-                        <iframe  title = "bouncing-balls" scrolling="no" frameBorder="0" src="https://editor.p5js.org/ejmeitz/embed/Ggf2mFF9o"></iframe>
-                    </div>
-                    <div className = "animation">
-                        <h2 className = "title">Bouncing Balls</h2> 
-                        <iframe  title = "bouncing-balls" scrolling="no" frameBorder="0" src="https://editor.p5js.org/ejmeitz/embed/Ggf2mFF9o"></iframe>
-                    
-                    </div>
-                    <div className = "animation">
-                        <h2 className = "title">Bouncing Balls</h2>
-                        <iframe  title = "bouncing-balls" scrolling="no" frameBorder="0" src="https://editor.p5js.org/ejmeitz/embed/Ggf2mFF9o"></iframe>
-                    </div>
+                        <div className = "animation a">
+                            <iframe samesite = "None Secure"  title = "bouncing-balls" width="100%" height="100%" frameBorder="0" scrolling="no" id="ballFrame"  src="https://editor.p5js.org/ejmeitz/embed/Ggf2mFF9o"></iframe>
+                        </div>
+                    </Container>
+                    <Container>
+                        <h2 className = "title">TicTacToe</h2> 
+                        <div className = "animation b">
+                            <iframe  samesite = "None Secure" title = "tictactoe" width="100%" height="100%" frameBorder="0" scrolling="no" id="tttFrame" src="https://editor.p5js.org/ejmeitz/embed/6Ll3mkIKB"></iframe>
+                        </div>
+                    </Container>
                 </main>
-            </div>
         </FunStyles>
     </>
         )
