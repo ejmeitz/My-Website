67ed9e06b2863dfc3ec28df0afdd07132825858c
Ethan Meitz <ejmeitz1@gmail.com>
2020-08-14 10:05:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true})); 
 app.use(bodyParser.json());
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 let URI;
 if(process.env.NODE_ENV === 'production'){
